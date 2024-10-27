1fb9133130f3d49a2cbae35fac08bad3338c2525
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-27 13:15:20 +0200

    setting-up supabase backend with the app

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://gegbxwlhneavclvrxvdb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZ2J4d2xobmVhdmNsdnJ4dmRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwMjM2MTEsImV4cCI6MjA0NTU5OTYxMX0._nknbw1kUcha6rqbsUy6zvw65KBZEJEOygkgCMLYiy0";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
