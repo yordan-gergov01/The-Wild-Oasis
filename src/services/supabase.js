eb533811a34c9c36824e4200f5a7477412fd8811
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-28 14:43:47 +0200

    uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://gegbxwlhneavclvrxvdb.supabase.co";
+
+export const supabaseUrl = "https://gegbxwlhneavclvrxvdb.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZ2J4d2xobmVhdmNsdnJ4dmRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAwMjM2MTEsImV4cCI6MjA0NTU5OTYxMX0._nknbw1kUcha6rqbsUy6zvw65KBZEJEOygkgCMLYiy0";
 const supabase = createClient(supabaseUrl, supabaseKey);
