005d546c29ca68130ec50d8871bd55627a51c6e2
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-29 10:01:33 +0200

    detecting a click outside the modal and make custom hook

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+
+export function useOutsideClick(close, listenCapturing = true) {
+  const ref = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) close();
+      }
+      document.addEventListener("click", handleClick, listenCapturing);
+
+      return () =>
+        document.removeEventListener("click", handleClick, listenCapturing);
+    },
+    [close, listenCapturing]
+  );
+
+  return ref;
+}
