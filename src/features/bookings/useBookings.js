0752c4e32fa39a2ce06b6ad903b31e7cd178f559
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-30 14:22:01 +0200

    add sorting bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -12,13 +12,18 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue };
 
+  //SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isPending, error, bookings };
