beb8179fc3a2de9c130b6b3e0c76e798c99c2625
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-31 12:05:35 +0200

    implement user logout

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -15,7 +15,7 @@ export function useLogin() {
       }),
     onSuccess: (user) => {
       queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("ERROR", err);
