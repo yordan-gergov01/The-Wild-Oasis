2d4ccd921f4fc34b9b3e4b16d98307a9348ac765
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-31 16:55:36 +0200

    building the sign-up form

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -14,7 +14,7 @@ export function useLogin() {
         password,
       }),
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
