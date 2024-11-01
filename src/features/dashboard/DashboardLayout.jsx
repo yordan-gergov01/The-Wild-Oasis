21a9421a3f1fc2b58a1c0b3adb34501de188c1f3
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-01 09:45:30 +0200

    computing recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,11 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { isPending: isLoading1, bookings } = useRecentBookings();
+  const { isPending: isLoading2, stays, confirmedStays } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
   return <StyledDashboardLayout></StyledDashboardLayout>;
 }
 
