dba9ebb220bbd3862509c6e0cb1a2e13c7581d8d
yordan-gergov01 <yordansoftskills@gmail.com>
2024-11-01 18:25:51 +0200

    show the error if booking is not exist

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -36,17 +36,16 @@ const Box = styled.div`
 function ErrorFallback({ error, resetErrorBoundary }) {
   return (
     <>
-      <GlobalStyles>
-        <StyledErrorFallback>
-          <Box>
-            <Heading as="h1">Something went wrong 🤔</Heading>
-            <p>{error.message}</p>
-            <Button size="large" onClick={resetErrorBoundary}>
-              Try again
-            </Button>
-          </Box>
-        </StyledErrorFallback>
-      </GlobalStyles>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong 🤔</Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
     </>
   );
 }
